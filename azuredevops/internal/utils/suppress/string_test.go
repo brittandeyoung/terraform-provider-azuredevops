8458500e6fd7371d38180fb6a414d04043920f2e
Brittan DeYoung <bdeyoung@hagerty.com>
2026-03-12 08:46:34 -0400

    Add unit tests for WhitespaceJsonDifference suppress function

--- azuredevops/internal/utils/suppress/string_test.go
+++ azuredevops/internal/utils/suppress/string_test.go
@@ -52,3 +52,57 @@ func TestCaseDifference(t *testing.T) {
 		})
 	}
 }
+
+func TestWhitespaceJsonDifference(t *testing.T) {
+	cases := []struct {
+		Name     string
+		StringA  string
+		StringB  string
+		Suppress bool
+	}{
+		{
+			Name:     "empty",
+			StringA:  "",
+			StringB:  "",
+			Suppress: true,
+		},
+		{
+			Name:     "empty vs json string",
+			StringA:  "{\"priority\": 2, \"area\": \"Business\"}",
+			StringB:  "",
+			Suppress: false,
+		},
+		{
+			Name:     "different json string",
+			StringA:  "{\"priority\": 2, \"area\": \"Business\"}",
+			StringB:  "{\"area\": \"Business\", \"priority\": 2, \"random\": \"text\"}",
+			Suppress: false,
+		},
+		{
+			Name:     "same json string",
+			StringA:  "{\"priority\": 2, \"area\": \"Business\"}",
+			StringB:  "{\"priority\": 2, \"area\": \"Business\"}",
+			Suppress: true,
+		},
+		{
+			Name:     "same json string whitespace difference",
+			StringA:  "{\"priority\": 2, \"area\": \"Business\"}",
+			StringB:  "{\"priority\": 2,            \"area\"  : \"Business\"}",
+			Suppress: true,
+		},
+		{
+			Name:     "same json string different order",
+			StringA:  "{\"priority\": 2, \"area\": \"Business\"}",
+			StringB:  "{\"area\": \"Business\", \"priority\": 2}",
+			Suppress: true,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			if WhitespaceJsonDifference("test", tc.StringA, tc.StringB, nil) != tc.Suppress {
+				t.Fatalf("Expected WhitespaceJsonDifference to return %t for '%q' == '%q'", tc.Suppress, tc.StringA, tc.StringB)
+			}
+		})
+	}
+}
